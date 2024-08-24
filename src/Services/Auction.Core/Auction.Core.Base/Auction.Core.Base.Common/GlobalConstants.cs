de8072fc6e44965e74386f3c3ac9e2d4b2bd240a
engKaya <ibrahimsabitkaya@gmail.com>
2024-08-25 00:53:02 +0300

    setting up elastic

---
+++ src/Services/Auction.Core/Auction.Core.Base/Auction.Core.Base.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Auction.Core.Base.Common
+{
+    public static class GlobalConstants
+    {
+
+    }
+
+    public static class EnviromentConstants
+    {
+        public const string MODULE_NAME = "MODULE_NAME";
+        public const string ENVIRONMENT = "ASPNETCORE_ENVIRONMENT";
+    }
+}
--- src/Services/Auction.Core/Auction.Core.Logging/Auction.Core.Logging.Service/LoggingServiceConfigurer.cs
+++ src/Services/Auction.Core/Auction.Core.Logging/Auction.Core.Logging.Service/LoggingServiceConfigurer.cs
@@ -1,4 +1,6 @@
-﻿using Auction.Core.Base.Common.Interfaces.ServiceConfigurers;
+﻿using Auction.Core.Base.Common;
+using Auction.Core.Base.Common.Helpers;
+using Auction.Core.Base.Common.Interfaces.ServiceConfigurers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -12,35 +14,49 @@ namespace Auction.Core.Logging.Service
         public void Configure(IServiceCollection services)
         {
             var config = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+            var moduleName = Environment.GetEnvironmentVariable(EnviromentConstants.MODULE_NAME);
+            var environment = Environment.GetEnvironmentVariable(EnviromentConstants.ENVIRONMENT);
+
 
             services.AddSerilog(cfg =>
             {
                 cfg.MinimumLevel.Information()
-                    .WriteTo.Console()
-                    .WriteTo.File($"log{DateTime.Now.Ticks}.txt", rollingInterval: RollingInterval.Day)
                     .Enrich.FromLogContext()
-                    .WriteTo.Elasticsearch(
-                        new ElasticsearchSinkOptions(new Uri(config["ElasticConfiguration:Uri"] ?? "http://localhost:9200"))
-                        {
-                            IndexFormat = $"{config["ApplicationName"]?.ToLower().Replace(".", " - ")}- logs -{DateTime.UtcNow}",
-                            AutoRegisterTemplate = true,
-                            DetectElasticsearchVersion = true,
-                            ConnectionTimeout = TimeSpan.FromSeconds(20),
-                            AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
-                            NumberOfShards = 2,
-                            NumberOfReplicas = 1,
-                            FailureCallback = (l, ex) => Console.WriteLine($"Unable to submit event:{l}|Exception: {ex}"),
-                            EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
-                                                               EmitEventFailureHandling.WriteToFailureSink |
-                                                               EmitEventFailureHandling.RaiseCallback,
-                            CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
-                            DeadLetterIndexName = "deadletterindex",
-                            RegisterTemplateFailure = RegisterTemplateRecovery.IndexAnyway,
-                            PipelineName = "pipeline",
-                        })
+                    .Enrich.WithProcessId()
+                    .Enrich.WithProperty("ModuleName", moduleName)
+                    .Enrich.WithMachineName()
+                    .WriteTo.Debug()
+                    .WriteTo.Console()
+                    .WriteTo.File($"serilogs/log{DateTime.Now.Ticks}.txt", rollingInterval: RollingInterval.Day)
+                    .WriteTo.Elasticsearch(ConfigureElasticSink(moduleName, config, environment))
                     .Enrich.WithProperty("Environment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
                     .ReadFrom.Configuration(config);
             });
         }
-    }
-}
+
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(string moduleName, IConfiguration configuration, string environment)
+        {
+            var uri = configuration["ElasticConfiguration:Uri"];
+            string indexFormat = $"{moduleName.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}";
+
+            return new ElasticsearchSinkOptions(new Uri(uri))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = indexFormat.RemoveTurkishChars(),
+                DetectElasticsearchVersion = true,
+                ConnectionTimeout = TimeSpan.FromSeconds(20), 
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                NumberOfShards = 2,
+                NumberOfReplicas = 1,
+                FailureCallback = (l, ex) => Console.WriteLine($"Unable to submit event:{l}|Exception: {ex}"),
+                EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                                               EmitEventFailureHandling.WriteToFailureSink |
+                                                               EmitEventFailureHandling.RaiseCallback,
+                CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: true),
+                DeadLetterIndexName = "deadletterindex",
+                RegisterTemplateFailure = RegisterTemplateRecovery.IndexAnyway, 
+            };
+        }
+    } 
+} 
