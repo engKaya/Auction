7ce0cda51a352b8711607f1a8d3ecee8e4ec873c
engKaya <ibrahimsabitkaya@gmail.com>
2024-09-15 22:44:59 +0300

    ..

---
+++ src/Services/Auction.User/Auction.User.Domain.Service/Handlers/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,43 @@
+﻿using Auction.Core.Base.Common.Infastructure;
+using MediatR;
+
+namespace Auction.User.Domain.Service.Handlers.Commands.RegisterUser
+{
+    public class RegisterUserCommand : BaseRequest, IRequest
+    {
+        private string _email = string.Empty;
+        private string _password = string.Empty;
+        private string _name = string.Empty;
+        private string _surname = string.Empty;
+        private string _phoneNumber = string.Empty;
+         
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            set => _password = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = value;
+        }
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        } 
+    }
+}
---
+++ src/Services/Auction.User/Auction.User.Domain.Service/Handlers/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Auction.Core.Logging.Common.Interfaces;
+using Auction.Core.Middleware.Service.MediatR;
+
+namespace Auction.User.Domain.Service.Handlers.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler : BaseRequestHandler<RegisterUserCommand>
+    {
+        public RegisterUserCommandHandler(ITraceService trace) : base(trace) 
+        { 
+            
+        }
+
+        protected override Task HandleRequest(RegisterUserCommand request, CancellationToken cancellationToken) 
+        { 
+            Console.WriteLine("RegisterUserCommandHandler");
+
+            return Task.CompletedTask;
+        }
+     }
+}
