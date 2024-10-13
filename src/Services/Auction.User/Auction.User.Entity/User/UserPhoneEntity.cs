56f97f19cdad2b13cf2de2581667b95c595c194a
engKaya <ibrahimsabitkaya@gmail.com>
2024-10-13 17:47:45 +0300

    .

---
+++ src/Services/Auction.User/Auction.User.Entity/User/UserPhoneEntity.cs
@@ -0,0 +1,18 @@
+﻿using Auction.Core.Repository.Common.DataBase;
+
+namespace Auction.User.Entity.User
+{
+    public class UserPhoneEntity : BaseEntity
+    {
+        private int _id;
+        private int _userId;
+        private string _phone = string.Empty;
+        private bool _valid;
+
+        public int Id { get => _id; set => _id = value; } 
+        public int UserId { get => _userId; set => _userId = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+        public bool Valid { get => _valid; set => _valid = value; }
+        public UserEntity User { get; set; } = null!;
+    }
+}
---
+++ src/Services/Auction.User/Auction.User.Entity/User/UserPhoneEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auction.User.Entity.User
+{
+    public class UserPhoneEntityConfiguration : IEntityTypeConfiguration<UserPhoneEntity>
+    { 
+        public void Configure(EntityTypeBuilder<UserPhoneEntity> builder)
+        {
+            builder.ToTable("USER_PHONE");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Id).HasColumnName("Id");
+            builder.Property(x => x.UserId).HasColumnName("UserId");
+            builder.Property(x => x.Phone).HasColumnName("Phone");
+            builder.Property(x => x.Valid).HasColumnName("Valid"); 
+            builder.HasOne(x => x.User).WithMany(x => x.UserPhone).HasForeignKey(x => x.UserId);
+        }
+    }
+}
